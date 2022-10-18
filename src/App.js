ba558e2d03fbdc0dab2467951ce303a5a7537582
Vivis <vivian@contateagora.com.br>
2022-10-18 14:13:17 -0300

    Migra html dos componentes restantes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import Home from "./components/pages/Home";
+//import Home from "./components/pages/Home";
+//import UserBlog from "./components/pages/UserBlog";
+//import Users from "./components/pages/Users";
+import UserPostForm from "./components/pages/UserPostForm";
+
 import "./styles/normalize.css";
 import "./styles/fontawesome.min.css";
 import "./styles/main.css";
 
 function App() {
-  return <Home />;
+  return <UserPostForm />;
 }
 
 export default App;
